5ef81f4f78dc919f1911252d6a18582deb06ec92
Munna Kumar Pandit <96653272+Munna764592@users.noreply.github.com>
2024-10-21 14:03:23 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-    const apiKey = "AIzaSyBm91vHhnByN054IfDGKslmkk445TgXG_8";
+    const apiKey = "apikey";
     chrome.storage.local.set({ api_key: apiKey }, () => {
         // console.log('API key has been stored');
     });
